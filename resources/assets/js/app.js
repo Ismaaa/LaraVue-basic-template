0136928254cbb1a55ef0a99ea1304e5cbbbb3917
Ismaa <didouhismael@gmail.com>
2017-06-12 20:59:37 +0200

    Conf bàsica Vue i store.js, routes.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,50 @@
+/**
+ * Importem el core
+ */
+import Vue from 'vue'
 
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * Router i Vuex
  */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
+import VueRouter from 'vue-router'
+import store from './store'
 
 /**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
+ * Nom de la instància
+ * @type {[instance]}
  */
+window.Vue = Vue;
 
-Vue.component('example', require('./components/Example.vue'));
+/**
+ * Importació de components
+ */
+import router from './routes' // Rutes
 
+/**
+ * Assignem els headers al resource
+ */
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.getElementsByName('csrf-token')[0].getAttribute('content')
+
+/**
+ * Exportació de la instància
+ */
+export default Vue
+
+/**
+ * Creació de la instància Vue amb nom app
+ * @type {Vue}
+ */
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    store,
+    router
+});
+
+/**
+ * Control de les rutes amb Vue-Router
+ * @param  {[object]} (to, from, next [Cap a on va, d'on ve, i la funció next]
+ * @return {[router]} [Redireccionem a una ruta]
+ */
+router.beforeEach((to, from, next) => {
+	next()
 });
